e31893b53b324a47ee2a15fc31d8dcd531422a07
Forb Yuan <f0rb@163.com>
2023-11-27 15:57:22 +0800

    build select statement

--- src/query/builder.go
+++ src/query/builder.go
@@ -6,6 +6,12 @@ import (
 	"strings"
 )
 
+type EntityMetadata struct {
+	TableName string
+	Columns   []string
+	ColStr    string
+}
+
 func BuildConditions(query interface{}) (string, []any) {
 	refType := reflect.TypeOf(query)
 	rv := reflect.ValueOf(query)
@@ -28,5 +34,30 @@ func BuildConditions(query interface{}) (string, []any) {
 }
 
 func IsValidValue(value reflect.Value) bool {
-	return (value.Type().Name() == "bool" && value.Bool()) || !value.IsNil()
+	if value.Type().Name() == "bool" {
+		return value.Bool()
+	} else {
+		return !value.IsNil()
+	}
+}
+
+func BuildEntityMetadata(entity interface{}) EntityMetadata {
+	refType := reflect.TypeOf(entity)
+	conditions := make([]string, refType.NumField())
+	for i := 0; i < refType.NumField(); i++ {
+		field := refType.Field(i)
+		conditions[i] = field.Name
+	}
+	return EntityMetadata{
+		TableName: strings.TrimSuffix(refType.Name(), "Entity"),
+		Columns:   conditions,
+		ColStr:    strings.Join(conditions, ", "),
+	}
+}
+
+func (em *EntityMetadata) BuildSelect(query interface{}) (string, []any) {
+	conditions, args := BuildConditions(query)
+	return "SELECT " + em.ColStr +
+		" FROM " + em.TableName +
+		" WHERE " + conditions, args
 }
--- src/query/builder_test.go
+++ src/query/builder_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+type UserEntity struct {
+	id    int
+	score int
+	memo  string
+}
+
 type UserQuery struct {
 	idGt     *int
 	scoreLt  *int
@@ -23,6 +29,19 @@ func TestBuild(t *testing.T) {
 			t.Errorf("Expected: %s, but got %s", expect, actual)
 		}
 		if len(args) != 1 || args[0] != int64(5) {
+			t.Errorf("Args are not expected: %s", args)
+		}
+	})
+
+	t.Run("Build Select Statement", func(t *testing.T) {
+		em := BuildEntityMetadata(UserEntity{})
+		query := UserQuery{idGt: intPtr(5), scoreLt: intPtr(60)}
+		actual, args := em.BuildSelect(query)
+		expect := "SELECT id, score, memo FROM User WHERE id > ? AND score < ?"
+		if actual != expect {
+			t.Errorf("\nExpected: %s\nBut got : %s", expect, actual)
+		}
+		if len(args) != 2 || args[0] != int64(5) || args[1] != int64(60) {
 			t.Errorf("Args are not expected: %s", args)
 		}
 	})
