1859e9b6663c6f3d4efed2b575e667f08db01be9
Forb Yuan <f0rb@163.com>
2023-11-27 10:37:45 +0800

    BuildConditions return sql and args

--- src/query/builder.go
+++ src/query/builder.go
@@ -6,20 +6,25 @@ import (
 	"strings"
 )
 
-func BuildConditions(query interface{}) string {
+func BuildConditions(query interface{}) (string, []any) {
 	refType := reflect.TypeOf(query)
 	rv := reflect.ValueOf(query)
-	cnt := 0
+	cnt, argCnt := 0, 0
 	conditions := make([]string, refType.NumField())
+	args := make([]any, refType.NumField(), 2*refType.NumField())
 	for i := 0; i < refType.NumField(); i++ {
 		field := refType.Field(i)
 		value := rv.FieldByName(field.Name)
 		if IsValidValue(value) {
 			conditions[cnt] = suffix.Process(field.Name)
 			cnt++
+			if value.Type().String() == "*int" {
+				args[argCnt] = reflect.Indirect(value).Int()
+				argCnt++
+			}
 		}
 	}
-	return strings.Join(conditions[0:cnt], " AND ")
+	return strings.Join(conditions[0:cnt], " AND "), args[0:argCnt]
 }
 
 func IsValidValue(value reflect.Value) bool {
--- src/query/builder_test.go
+++ src/query/builder_test.go
@@ -17,10 +17,13 @@ func intPtr(o int) *int {
 func TestBuild(t *testing.T) {
 	t.Run("Build Where Clause", func(t *testing.T) {
 		query := UserQuery{idGt: intPtr(5), memoNull: true}
-		actual := BuildConditions(query)
+		actual, args := BuildConditions(query)
 		expect := "id > ? AND memo IS NULL"
 		if actual != expect {
 			t.Errorf("Expected: %s, but got %s", expect, actual)
+		}
+		if len(args) != 1 || args[0] != int64(5) {
+			t.Errorf("Args are not expected: %s", args)
 		}
 	})
 
