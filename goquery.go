32f5af936ca04d07ef5405053ea2bebf0e65c29b
Forb Yuan <f0rb@163.com>
2023-12-13 17:56:03 +0800

    export BuildDataAccess

--- goquery.go
+++ goquery.go
@@ -3,10 +3,15 @@ package goquery
 import (
 	"database/sql"
 	"github.com/doytowin/goquery/core"
+	"github.com/doytowin/goquery/rdb"
 	"github.com/doytowin/goquery/web"
 )
 
 type GoQuery = core.GoQuery
+
+type PageQuery = core.PageQuery
+
+type DataAccess[E any] core.DataAccess[E]
 
 func BuildController[E any, Q GoQuery](
 	prefix string,
@@ -17,4 +22,8 @@ func BuildController[E any, Q GoQuery](
 	return &web.RestService[E, Q]{
 		Service: web.BuildService(prefix, db, createEntity, createQuery),
 	}
+}
+
+func BuildDataAccess[E any](createEntity func() E) DataAccess[E] {
+	return rdb.BuildDataAccess[E](createEntity)
 }
--- goquery_test.go
+++ goquery_test.go
@@ -1,6 +1,7 @@
 package goquery
 
 import (
+	. "github.com/doytowin/goquery/test"
 	log "github.com/sirupsen/logrus"
 	"testing"
 )
@@ -8,4 +9,7 @@ import (
 func TestBuild(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
 
+	t.Run("Export Interface", func(t *testing.T) {
+		_ = BuildDataAccess[UserEntity](func() UserEntity { return UserEntity{} })
+	})
 }
