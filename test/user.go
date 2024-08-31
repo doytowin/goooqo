74e0035e876163931deaafda28762abe79a7ebe6
Forb Yuan <f0rb@163.com>
2024-08-31 18:28:36 +0800

    add The Clear BSD License

--- test/user.go
+++ test/user.go
@@ -1,3 +1,13 @@
+/*
+ * The Clear BSD License
+ *
+ * Copyright (c) 2024, DoytoWin, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package test
 
 import . "github.com/doytowin/goooqo/core"
